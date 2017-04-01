f0035e0ce50f2d1d760434cb92014c8c304772bd
elevenBeans <elevenbeansf2e@gmail.com>
2017-04-01 17:40:27 +0800

    opt title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,8 +9,13 @@ import Pannel from './pannel';
 
 const App = () => (
     <div>
-    	<h1>ReactJS Game of Life 
-    		<a target="_blank" href="https://www.math.cornell.edu/~lipa/mec/lesson6.html"> (Click to learn more)</a>
+    	<h1>ReactJS Game of Life &nbsp;
+    		<a
+    			target="_blank"
+    			href="https://www.math.cornell.edu/~lipa/mec/lesson6.html"
+    		>
+					(Click to learn more)
+    		</a>
     	</h1>
 			<Pannel />
     </div>
